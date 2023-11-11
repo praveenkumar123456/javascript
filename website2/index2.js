f6120648c676eb648dfa3c5aa29ed53fb09e9789
praveenkumar123456 <praveenkumar107pk@gmail.com>
2023-11-11 23:50:39 +0530

    math function

---
+++ website2/index2.js
@@ -0,0 +1,22 @@
+
+let x=2.78;
+let y=5;
+let z=9;
+let maximum;
+let minimum;
+// x=Math.round(x);
+// x=Math.floor(x);
+// x=Math.ceil(x);
+// x=Math.pow(x);
+// x=Math.sqrt(x);
+// x=Math.abs(x);
+
+
+maximum=Math.max(x,y,z);
+
+minimum=Math.min(x,y,z);
+
+console.log(minimum);
+console.log(maximum);
+
+
