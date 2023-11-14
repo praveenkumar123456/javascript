862d8082129f46164730aeb78a127833fc5054c0
praveenkumar123456 <praveenkumar107pk@gmail.com>
2023-11-14 11:56:21 +0530

    string function

---
+++ website5/index5.js
@@ -0,0 +1,8 @@
+let username="praveen kumar";
+
+console.log(username.length);
+console.log(username.charAt(0));
+console.log(username.indexOf("k"));
+console.log(username.lastIndexOf("a"));
+console.log(username.toUpperCase());
+console.log(username.toLowerCase());
