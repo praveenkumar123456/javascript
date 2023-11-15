ea0170d638f3b6a63a118d6a1d3b28e8d778cb8d
praveenkumar123456 <praveenkumar107pk@gmail.com>
2023-11-15 15:35:40 +0530

    if else in js

---
+++ website6.html/index6.js
@@ -0,0 +1,19 @@
+let age=65;
+
+if(age>=65)
+{
+    console.log("you are senior citizen");
+}
+
+else if(age>=18)
+{
+    console.log("you are an adult");
+}
+
+else if(age<=0)
+{
+    console.log("you are not born yet");
+}
+else{
+    console.log("you are child");
+}
