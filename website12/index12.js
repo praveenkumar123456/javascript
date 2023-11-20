904142e092a053bad7833017e5ea2fb5c79e1a5a
praveenkumar123456 <praveenkumar107pk@gmail.com>
2023-11-20 19:30:48 +0530

    array and its function

---
+++ website12/index12.js
@@ -0,0 +1,24 @@
+let fruits=["apple","banana","papayaa"];
+
+console.log(fruits[0]);
+
+//fruits[2]="coconut";
+
+// console.log(fruits);
+
+fruits.push("lemon");
+
+fruits.unshift("mango");//add element in brginning
+fruits.shift();//remove element from beginning
+
+
+let length=fruits.length;
+console.log("length is:=> ",length);
+
+let index= fruits.indexOf("apple");
+
+console.log("index of apple is:=> ",index);
+
+console.log(fruits)
+
+
