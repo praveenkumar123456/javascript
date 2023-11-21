7db082b821d22ad56c28f8e3d7415c21ccdbae84
praveenkumar123456 <praveenkumar107pk@gmail.com>
2023-11-21 15:23:41 +0530

    spread operator

---
+++ website14/index14.js
@@ -0,0 +1,13 @@
+//spread operator ...
+
+
+let class1=["apple","banana","cat"];
+
+let class2=["dog","elephant","fish"];
+
+console.log(...class1);
+
+
+class1.push(...class2);
+
+console.log(...class1);
